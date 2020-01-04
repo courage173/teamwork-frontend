9be977fd3522d1ed4bc80515f1e086f5744d79fb
courage173 <courageosemwengie@gmail.com>
2020-01-04 23:37:14 +0100

    addded engine to package.json

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/button'
+import {Button} from '@material-ui/core'
 import {connect} from "react-redux"
 import CircularProgress from '@material-ui/core/CircularProgress'; 
 import {login} from '../../actions/auth'
