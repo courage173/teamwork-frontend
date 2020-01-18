1bf48c849723c8d399410305b781acc839436b48
courage173 <courageosemwengie@gmail.com>
2020-01-18 22:32:36 +0100

    refactor(made some changes in the homepage and delete page)

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,10 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h1>About us</h1>
+                <h3>Teamwork is an internal social network for employees of an organization. The goal of this application is to facilitate interaction between colleagues and promote internal bonding. This is the Frontend of the application done in ReactJs</h3>
+                <br />
+                <hr />
+                <h3> Login with Username- test@test.com and Password - testing123 to access the admin priviledges</h3>
             </div>
         )
     }
